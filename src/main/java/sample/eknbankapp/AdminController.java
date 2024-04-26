73ecdb700851cd9cb619fb9255f5013b73df5904
Enes Gunel <gunelenes@hotmail.com>
2024-04-26 21:08:32 +0300

    Correction

--- src/main/java/sample/eknbankapp/AdminController.java
+++ src/main/java/sample/eknbankapp/AdminController.java
@@ -1,6 +1,7 @@
 package sample.eknbankapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -9,7 +10,7 @@ import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class AdminController {
+public class AdminController implements Initializable {
 
     @FXML
     private Label adminNameLabel;
